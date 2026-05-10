689ed7d2f3c0c0f3dbf47dfff73fa1c791446b74
Vaibhav Shukla <kingshukla221@gmail.com>
2026-05-10 20:41:29 +0530

    Socket.io Added in Server

--- server.js
+++ server.js
@@ -3,16 +3,49 @@ import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 
-const app = express();
+import http from "http";
+import { Server } from "socket.io";
+
 dotenv.config();
+
+const app = express();
+
+// create http server
+const server = http.createServer(app);
+
+// create socket server
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
 app.use(express.json());
 
 // connect database
 connectDB();
 
-// routes
+// normal routes
 app.use("/api", userRoutes);
 
-app.listen(process.env.PORT, () => {
+// socket connection
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+
+  // receive message
+  socket.on("message", (data) => {
+    console.log("Message:", data);
+
+    // send to all users
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
+});
+
+// IMPORTANT
+server.listen(process.env.PORT, () => {
   console.log(`Server running on localhost:${process.env.PORT}`);
 });
